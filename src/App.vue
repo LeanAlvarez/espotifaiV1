d921a0cc5af5023e999ca78d5e9bb9c84a99077b
Lean Alvarez <leandro.alvarez8727@gmail.com>
2019-05-22 16:48:50 -0300

    fixes de url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #app
-  img(src='./assets/logo.png')
+  img(src='https://leanalvarez.github.io/espotifaiV1/dist/logo.png')
   h1 Espotifai
 
   select(v-model="selctedCountry")
